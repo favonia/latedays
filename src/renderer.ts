a67bbb8aeb49b61f43c27aecf092d4674b4b5f21
favonia <favonia@gmail.com>
2022-11-05 10:26:43 -0500

    style: apply prettier

--- src/renderer.ts
+++ src/renderer.ts
@@ -5,42 +5,49 @@ import Handlebars from "handlebars";
 import baseTemplate from "../templates/base.html";
 
 type updateSummaryRender = {
-  spendLiteral: string,
+  spendLiteral: string;
   usageLiteral: Array<{
-    assignment: string,
-    comments: string,
-  }>,
-  remainingLiteral: string,
-}
+    assignment: string;
+    comments: string;
+  }>;
+  remainingLiteral: string;
+};
 
-export function getEmailSubject(reqAct: string, res: Response) : string {
-  if (reqAct === "summary")
-    return literal.summary.subject();
-  return literal.default.subject(reqAct, res.assignment, res.success, res.state.new?.deadline);
+export function getEmailSubject(reqAct: string, res: Response): string {
+  if (reqAct === "summary") return literal.summary.subject();
+  return literal.default.subject(
+    reqAct,
+    res.assignment,
+    res.success,
+    res.state.new?.deadline
+  );
 }
 
 export function renderHTML(req: form.Request, res: Response): string {
   var template = Handlebars.compile(baseTemplate);
 
-  const spendLiteral=(res.updateSummary.usage.length>0) ?
-    `You have applied these late day(s):`:
-    `You have not spent any late day`;
+  const spendLiteral =
+    res.updateSummary.usage.length > 0
+      ? `You have applied these late day(s):`
+      : `You have not spent any late day`;
   var summary = {
     spendLiteral: spendLiteral,
     remainingLiteral: `Remaining late day(s): ${res.updateSummary.remainingDays}`,
   } as updateSummaryRender;
-  summary.usageLiteral= (res.updateSummary.usage.map(u=>{
+  summary.usageLiteral = res.updateSummary.usage.map((u) => {
     const container = {} as any;
-    container.comments = `${u.used + u.free} day(s)` + (u.free > 0 ? ` (including ${u.free} free late day(s))` : ``);
-    container.assignment =  u.assignment;
+    container.comments =
+      `${u.used + u.free} day(s)` +
+      (u.free > 0 ? ` (including ${u.free} free late day(s))` : ``);
+    container.assignment = u.assignment;
     return container;
-  }))
+  });
 
   var placeholders = {
-    "greetings": `Hi ${req.id}`,
-    "approval": res.success,
-    "body": res.comments?.join(" "),
-    "summary": summary
+    greetings: `Hi ${req.id}`,
+    approval: res.success,
+    body: res.comments?.join(" "),
+    summary: summary,
   };
 
   return template(placeholders);
